304656a8e3c729649b51ca86729df70433ae8c51
nxteai <NathanielNtow@outlook.com>
2024-09-18 02:06:28 +0100

    Create Attempt1.kt

---
+++ Attempt1.kt
@@ -0,0 +1,12 @@
+package flashcards
+
+fun main() {
+    println("Card:")
+    val card = readln()
+    println("$card")
+    println("Definition:")
+    val definition = readln()
+    println("$definition")
+}
+
+// Displays information about a single card on the screen
